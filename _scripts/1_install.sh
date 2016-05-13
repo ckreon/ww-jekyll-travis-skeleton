cdbb532a5b5ebf24abff3da5032ee5e81fd926ce
Corey Kozlowski <ckreon@gmail.com>
2016-05-13 04:08:59 -0700

    Clean and update

--- _scripts/1_install.sh
+++ _scripts/1_install.sh
@@ -2,13 +2,13 @@
 set -x # Show the output of the following commands (useful for debugging)
 
 # Import the SSH deployment key
-openssl aes-256-cbc -K $encrypted_@@@@@@@@@@@@_key -iv $encrypted_@@@@@@@@@@@_iv -in _deploy/$JTS_KEYNAME.enc -out _deploy/$JTS_KEYNAME -d
+openssl aes-256-cbc -K $encrypted_@@@@@@@@@@@@_key -iv $encrypted_@@@@@@@@@@@@_iv -in _deploy/$JTS_KEYNAME.enc -out _deploy/$JTS_KEYNAME -d
 rm _deploy/$JTS_KEYNAME.enc
 chmod 600 _deploy/$JTS_KEYNAME
 mv _deploy/$JTS_KEYNAME ~/.ssh/$JTS_KEYNAME
 
 # Install Other Packages
-npm install
+#npm install
 
 # Install Zopfli
 git clone https://github.com/google/zopfli.git _zopfli
